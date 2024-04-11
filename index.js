830835ba495d37eb71070717e80da04c4dc07c86
Jaken <dy1011101@gmail.com>
2024-04-11 17:14:40 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+//Esqueci como faço para validar pegando os dados do input
+
+function botao1Click() {
+    let nomeUsuario = usuario
+
+    let emailUsuario = email
+    
+    let senhaUsuario = senha
+
+
+    if ( nomeUsuario == 'Deyvison' && emailUsuario == 'deyvison@email.com' && senhaUsuario == 'De123' ){
+    alert('Os seus dados estão corretos. Clique no botão para prosseguir.')
+}
+
+else {
+    alert('Os seus dados estão incorretos.')
+
+}
+
+}
+
